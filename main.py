9a55fac005288b6a7d866950f4002045742dbafd
Marcos Vinicius <vini.markinhos@gmail.com>
2024-03-20 16:43:24 -0300

    salvando exemplo de função / biblioteca

---
+++ main.py
@@ -0,0 +1,8 @@
+# Utilizando a função criada ((ordenar_lista_de_numeros)) no arquivo / biblioteca ((aula_func_algoritmo))
+
+from aula_func_algoritmo import ordenar_lista_de_numeros
+
+lista = [ 225, 25 , 65 , -626 , 1 , 5 ]
+
+nova_lista = ordenar_lista_de_numeros(numeros=lista)
+print(nova_lista)
